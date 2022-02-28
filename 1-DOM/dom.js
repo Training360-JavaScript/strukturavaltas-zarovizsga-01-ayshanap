879028a624b46498cfe0f9a70a8f0c21e7f3378e
Judit Orosz <orosz.judit@gmail.com>
2022-02-28 16:39:37 +0100

    1./2

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,9 @@
 function manipulateDom() {
     // 
+    const quotes = document.querySelectorAll('.quote');
+    quotes.forEach( item => item.style.border = 'blue');
+    quotes.forEach( item => item.style.background = 'lightblue');
+    quotes.forEach( item => item.style.fontStyle = 'italic');
 };
 
 export { manipulateDom };
